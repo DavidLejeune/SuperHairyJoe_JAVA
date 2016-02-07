60af3da4a510da48cfe590f478346bc4ef77ad1a
DavidLejeune <Lejeune.David@outlook.com>
2016-02-07 19:47:19 +0100

    Intro , setting up project & package

---
+++ SuperHairyJoe/src/com/dale/superhairyjoe/Game.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dale.superhairyjoe;
+
+/**
+ *
+ * @author David
+ * @about this is the Main class for the game
+ */
+public class Game {
+    
+    
+    
+}
