bb6fe10263c9e4200f993decb8a6e16bb9cdc4a8
DavidLejeune <Lejeune.David@outlook.com>
2016-02-07 20:28:15 +0100

    Frame created

--- SuperHairyJoe/src/com/dale/superhairyjoe/Game.java
+++ SuperHairyJoe/src/com/dale/superhairyjoe/Game.java
@@ -7,6 +7,7 @@ package com.dale.superhairyjoe;
 
 import java.awt.Canvas;
 import java.awt.Dimension;
+import javax.swing.JFrame;
 
 /**
  *
@@ -22,6 +23,8 @@ public class Game extends Canvas
     public static final int WIDTH = 270;
     public static final int HEIGHT = WIDTH / 14 * 10;
     public static final int SCALE = 4;
+    public static final String TITLE = "Super Hairy Joe";
+    
     
     public Game()
     {
@@ -33,5 +36,21 @@ public class Game extends Canvas
         
         
     }
+    
+    
+    public static void main(String[] args)
+    {
+        //Initialise this puppy
+        Game game = new Game();
+        JFrame frame = new JFrame(TITLE);
+        frame.add(game);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        
+    }
+    
     
 }
