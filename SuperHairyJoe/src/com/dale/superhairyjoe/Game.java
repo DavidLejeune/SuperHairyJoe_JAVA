97b6d07a4ee2325b7f0dd512ae50c202982b3ba8
DavidLejeune <Lejeune.David@outlook.com>
2016-02-07 20:06:29 +0100

    Resolution , scaled screen

--- SuperHairyJoe/src/com/dale/superhairyjoe/Game.java
+++ SuperHairyJoe/src/com/dale/superhairyjoe/Game.java
@@ -9,9 +9,15 @@ package com.dale.superhairyjoe;
  *
  * @author David
  * @about this is the Main class for the game
+ * 
  */
+
+
 public class Game {
     
+    public static final int WIDTH = 270;
+    public static final int HEIGHT = WIDTH / 14 * 10;
+    public static final int SCALE = 4;
     
     
 }
