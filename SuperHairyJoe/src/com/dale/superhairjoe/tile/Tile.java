78a4a9503c3da81d6692ff205fe59fe2f0661f01
DavidLejeune <Lejeune.David@outlook.com>
2016-02-07 22:43:43 +0100

    Super classes

---
+++ SuperHairyJoe/src/com/dale/superhairjoe/tile/Tile.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dale.superhairjoe.tile;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author David
+ */
+public class Tile {
+    
+        public int x, y;
+    public int width, height;
+    
+    public boolean solid;
+    
+    public int velX, velY;
+    
+    public Tile(int x, int y, int width, int height, boolean solid)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.solid = solid;
+        
+    }
+    
+    public void render(Graphics g)
+    {
+        
+    }
+    
+    public void tick()
+    {
+        x += velX;
+        y += velY;
+    }
+    
+    public int getX()
+    {
+        return x;
+    }
+    
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+    
+    public int getY()
+    {
+        return y;
+    }
+    
+    public void setY(int y)
+    {
+        this.y = y;
+    } 
+   
+     
+  
+    public void setvelX(int velX)
+    {
+        this.velX = velX;
+    }    
+    
+    public void setvelY(int velY)
+    {
+        this.velY = velY;
+    } 
+    
+    
+     public boolean isSolid()
+    {
+        return solid;
+    }
+    
+    public void setSolid(boolean solid)
+    {
+        this.solid = solid;
+    }
+    
+    
+}
